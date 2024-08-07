e12d714a78bae07a6c5f85c73801c50b0c3e53f7
ksenia <venina621@mail.ru>
2024-08-07 20:56:52 +0300

    import functions

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from masks .import get_mask_account, get_mask_card_number
+def get_mask_account_card(number: str) -> str:
+    """ Функция получает строку и маскирует счет/карту """
+    if len(number.split()[-1]) == 16:
+        new_number = card_number(number.split()[-1])
+        result = f"{number[:-16]}{new_number}"
+        # return result
+    elif len(number.split()[-1]) == 20:
+        new_number = get_mask_account_card(number.split()[-1])
+        result = f"{number[:-20]}{new_number}"
+    return result
+
+
+def get_new_data(old_data: str) -> str:
+    """ Функция принимает строку с датой и
+    выводит в формате dd.mm.yyyy """
+
+    data_slize = old_data[0:10].split("-")
+    return ".".join(data_slize[::-1])
