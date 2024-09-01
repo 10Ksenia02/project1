bd5b5d38ba2b8c16172d1297678eee31a20484b1
ksenia <venina621@mail.ru>
2024-09-01 20:53:32 +0300

    fixes file widget

--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,19 @@
 from src.masks import get_mask_account, get_mask_card
 
 
-def get_mask_account(number: str) -> str:
+def get_mask_account_card(number: str) -> str:
     """Функция получает строку и маскирует счет/карту"""
     if len(number.split()[-1]) == 16:
         new_number = get_mask_card(number.split()[-1])
-        result = f"{number[:-16]}{new_number}"
-        # return result
+        return f"{number[:-16]}{new_number}"
     elif len(number.split()[-1]) == 20:
         new_number = get_mask_account(number.split()[-1])
-        result = f"{number[:-20]}{new_number}"
-    return result
+        return f"{number[:-20]}{new_number}"
 
 
-if __name__ == "__main__":
-    print(get_mask_account("Счет 12345678901234567890"))
-    print(get_mask_account("Visa Classic 1234567890123456"))
+# if __name__ == "__main__":
+#     print(get_mask_account("Счет 12345678901234567890"))
+#     print(get_mask_account("Visa Classic 1234567890123456"))
     # должно вывестись в терминал
     # Счет **7890
     # Visa Classic 1234 56** **** 3456
